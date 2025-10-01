96e5a86f1ae98b37218d7ab213828302605fa2b1
SalmanMuhammadRidwan <salman.muhammadridwan@gmail.com>
2025-10-01 18:24:27 +0700

    penambahan emplicit intent

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -21,19 +21,25 @@ class MainActivity : AppCompatActivity() {
         val buttonToast = findViewById<Button>(R.id.button_toast)
         val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
         val buttonBrowser = findViewById<Button>(R.id.button_browser)
+        val buttonDial = findViewById<Button>(R.id.button_dial)
+        val buttonMap = findViewById<Button>(R.id.button_map)
+        val buttonEmail = findViewById<Button>(R.id.button_email)
 
+        // Tombol Counter
         buttonCountUp.setOnClickListener {
             mCount++
             Log.d("mCount", mCount.toString())
             mShowCount.text = mCount.toString()
         }
 
+        // Tombol Toast
         buttonToast.setOnClickListener {
             val tulisan: String = mShowCount.text.toString()
             val toast = Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG)
             toast.show()
         }
 
+        // Explicit Intent: Pindah ke Activity2
         buttonSwitchPage.setOnClickListener {
             try {
                 val intent = Intent(this, MainActivity2::class.java)
@@ -45,10 +51,35 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Implicit Intent: Buka Browser
         buttonBrowser.setOnClickListener {
             val intentbrowse = Intent(Intent.ACTION_VIEW)
             intentbrowse.data = Uri.parse("https://www.google.com/")
             startActivity(intentbrowse)
+        }
+
+        // Implicit Intent: Buka Dial Phone
+        buttonDial.setOnClickListener {
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:08123456789") // ganti dengan nomor lain
+            startActivity(dialIntent)
+        }
+
+        // Implicit Intent: Buka Lokasi Maps
+        buttonMap.setOnClickListener {
+            val mapIntent = Intent(Intent.ACTION_VIEW)
+            mapIntent.data = Uri.parse("geo:-6.175392,106.827153?q=Monas Jakarta")
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+        // Implicit Intent: Kirim Email
+        buttonEmail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:example@email.com")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Judul Email dari Aplikasi")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Halo, ini isi email otomatis dari aplikasi HelloToast.")
+            startActivity(emailIntent)
         }
     }
 }
